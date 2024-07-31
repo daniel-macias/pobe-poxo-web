2153b7318ba7cfa573b51d02b0719517dd20e1ac
daniel-macias <josedanielmr@gmail.com>
2024-07-30 20:13:36 -0600

    added new contact page, meta tags and basic logo

---
+++ app/contact/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="text-4xl font-bold mb-4">Contact</h1>
+      <p className="text-lg">
+        Explore the games developed by Pobe Poxo.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-Hola
+    <main className="flex min-h-screen flex-col items-center justify-center bg-white">
+      <div className="flex flex-col items-center">
+        <Image src="/assets/pobepoxo.png" alt="Pobe Poxo Logo" width={200} height={200} className="mb-8" />
+        <div className="text-center">
+          <p className="text-xl font-semibold text-gray-700">Coming Soon!</p>
+          <div className="mt-4 flex space-x-4">
+            <Link href="/about">
+              <span className="text-lg text-gray-700 hover:text-black">About</span>
+            </Link>
+            <Link href="/games">
+              <span className="text-lg text-gray-700 hover:text-black">Games</span>
+            </Link>
+            <Link href="/blog">
+              <span className="text-lg text-gray-700 hover:text-black">Blog</span>
+            </Link>
+            <Link href="/contact">
+              <span className="text-lg text-gray-700 hover:text-black">Contact</span>
+            </Link>
+          </div>
+        </div>
       </div>
     </main>
   );
