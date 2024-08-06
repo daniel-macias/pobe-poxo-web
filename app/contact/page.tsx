5a6493f6b091e5bdd1707125438472d8c4b37408
daniel-macias <josedanielmr@gmail.com>
2024-08-05 23:42:25 -0600

    added contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter, faTiktok, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const Contact = () => {
   return (
-    <div className="container mx-auto p-6">
+    <div className="container mx-auto p-6 text-center bg-white text-black min-h-screen flex flex-col justify-center items-center">
       <h1 className="text-4xl font-bold mb-4">Contact</h1>
-      <p className="text-lg">
-        Explore the games developed by Pobe Poxo.
-      </p>
+      <p className="text-lg mb-8">Follow us on social media!</p>
+      <div className="flex flex-col items-center space-y-4 mb-8">
+        <div className="flex flex-col items-center space-y-2">
+          <div className="flex items-center space-x-2">
+            <FontAwesomeIcon icon={faEnvelope} className="w-6 h-6" />
+            <a href="mailto:pobepoxo@gmail.com" className="text-lg text-black hover:underline">pobepoxo@gmail.com</a>
+          </div>
+          <div className="flex items-center space-x-2">
+            <FontAwesomeIcon icon={faTwitter} className="w-6 h-6" />
+            <a href="https://twitter.com/pobepoxo" className="text-lg text-black hover:underline">@pobepoxo</a>
+          </div>
+          <div className="flex items-center space-x-2">
+            <FontAwesomeIcon icon={faTiktok} className="w-6 h-6" />
+            <a href="https://tiktok.com/@pobepoxo" className="text-lg text-black hover:underline">@pobepoxo</a>
+          </div>
+          <div className="flex items-center space-x-2">
+            <FontAwesomeIcon icon={faInstagram} className="w-6 h-6" />
+            <a href="https://instagram.com/pobepoxo" className="text-lg text-black hover:underline">@pobepoxo</a>
+          </div>
+        </div>
+      </div>
+      <div className="text-center mt-8">
+        <p className="text-lg">For any inquiries and more information, visit:</p>
+        <a href="https://maciasreynaud.com" className="text-lg text-blue-500 hover:underline">maciasreynaud.com</a>
+      </div>
     </div>
   );
 };
