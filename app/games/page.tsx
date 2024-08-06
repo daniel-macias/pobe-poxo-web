2a09f58873cd5518cddc49fbb5a8ea8647cba5bc
daniel-macias <josedanielmr@gmail.com>
2024-08-06 00:05:46 -0600

    removed apos

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -5,7 +5,7 @@ const Games = () => {
     <div className="container mx-auto p-6 text-center bg-white text-black min-h-screen flex flex-col justify-center items-center">
       <h1 className="text-4xl font-bold mb-4">Our Games</h1>
       <p className="text-lg mb-4">
-        At Pobe Poxo, we're passionate about creating captivating mobile games that you can enjoy anywhere. But we're not stopping there—our sights are set on bringing greater gaming experiences to PC in the near future. Stay tuned!
+        At Pobe Poxo, we are passionate about creating captivating mobile games that you can enjoy anywhere. But we are not stopping there—our sights are set on bringing greater gaming experiences to PC in the near future. Stay tuned!
       </p>
       <p className="text-lg">
         Our first Android game is set to launch on September 1, 2024. Mark your calendars!
